ead0d837a1fd46458d674174a5a29658f212e64a
moisesguiaz <112730086+moisesguiaz@users.noreply.github.com>
2025-11-28 17:07:04 -0300

    Delete src/museum directory

--- src/museum/Administrator.java
+++
@@ -1,9 +0,0 @@
-package museum;
-
-public class Administrator extends User {
-    private static final long serialVersionUID = 1L;
-
-    public Administrator(String id, String username, String password) {
-        super(id, username, password);
-    }
-}
